989b0763e7d0d8b450a176f45372e7599d346b74
SaumyaPatil <saupatil10dec@gmail.com>
2025-10-28 14:29:10 +0530

    adding delete functionality

--- script.js
+++ script.js
@@ -4,17 +4,13 @@ let taskItems = document.getElementById("taskItems");
 let title = document.getElementById("title");
 let priorityDropdown = document.getElementById("priorityDropdown");
 let deadline = document.getElementById("deadline");
-
-let len = localStorageArray.length;
-let id = localStorageArray.length > 0 ? localStorageArray[len - 1][id] : 0;
+let id = localStorageArray.length;
+const btnIdMapping = new Map();
 
 addTask.onclick = function () {
-  // len = localStorageArray.length;
-  // id = len>0 ? localStorageArray[len-1][id] : 0;
-
-  // id = id + 1;
+  id = id + 1;
   let taskItem = {
-    // id: id,
+    itemId: id,
     itemTitle: title.value,
     itemPriorityDropdown: priorityDropdown.value,
     itemDeadline: deadline.value,
@@ -32,53 +28,76 @@ addTask.onclick = function () {
 };
 
 function updateDataOnUI() {
-  for (let index of localStorageArray) {
-    let taskCard = document.createElement("div");
-    taskCard.classList.add(
-      "bg-[white]",
-      "p-5",
-      "rounded-[5px]",
-      "w-[50%]",
-      "m-[auto]",
-      "mt-3",
-      "mb-3"
-    );
-    taskItems.appendChild(taskCard);
 
-    let cardDetails = document.createElement("div");
-    cardDetails.classList.add("flex", "flex-col", "justify-center");
-    let taskTitle = document.createElement("h1");
-    taskTitle.classList.add("mb-2", "font-bold", "text-[15px]");
-    taskTitle.textContent = index.itemTitle;
-    console.log(index.title);
-    cardDetails.appendChild(taskTitle);
+    // ADD OPERATION
+    for (let index of localStorageArray) {
+        let taskCard = document.createElement("div");
+        taskCard.classList.add(
+        "bg-[white]",
+        "p-5",
+        "rounded-[5px]",
+        "w-[50%]",
+        "m-[auto]",
+        "mt-3",
+        "mb-3",
+        "flex",
+        "justify-between"
+        );
+        taskItems.appendChild(taskCard);
 
-    let taskInfo = document.createElement("div");
-    let priority = document.createElement("span");
-    let date = document.createElement("span");
-    // let status = document.createElement("span");
+        let cardDetails = document.createElement("div");
+        cardDetails.classList.add("flex", "flex-col", "justify-center");
+        let taskTitle = document.createElement("h1");
+        taskTitle.classList.add("mb-2", "font-bold", "text-[15px]");
+        taskTitle.textContent = index.itemTitle;
+        console.log(index.title);
+        cardDetails.appendChild(taskTitle);
 
-    priority.textContent = index.itemPriorityDropdown;
-    console.log(index.priorityDropdown);
-    priority.classList.add(
-      "bg-orange-100",
-      "p-[2px]",
-      "rounded-[5px]",
-      "text-[red]",
-      "text-[12px]",
-      "text-[bold]"
-    );
-    date.textContent = index.itemDeadline;
-    console.log(index.deadline);
-    date.classList.add("text-[grey]", "text-[12px]");
+        let taskInfo = document.createElement("div");
+        let priority = document.createElement("span");
+        let date = document.createElement("span");
+        // let status = document.createElement("span");
 
-    taskInfo.classList.add("flex", "justify-between", "w-[200px]");
-    taskInfo.appendChild(priority);
-    taskInfo.appendChild(date);
+        priority.textContent = index.itemPriorityDropdown;
+        console.log(index.priorityDropdown);
+        priority.classList.add(
+        "bg-orange-100",
+        "p-[2px]",
+        "rounded-[5px]",
+        "text-[red]",
+        "text-[12px]",
+        "text-[bold]"
+        );
+        date.textContent = index.itemDeadline;
+        console.log(index.deadline);
+        date.classList.add("text-[grey]", "text-[12px]");
 
-    cardDetails.appendChild(taskInfo);
-    taskCard.appendChild(cardDetails);
-    taskItems.appendChild(taskCard);
-  }
+        taskInfo.classList.add("flex", "justify-between", "w-[200px]");
+        taskInfo.appendChild(priority);
+        taskInfo.appendChild(date);
+
+        cardDetails.appendChild(taskInfo);
+        taskCard.appendChild(cardDetails);
+        taskItems.appendChild(taskCard);
+
+        // DELETE OPERATION
+        let deleteTask = document.createElement("button");
+        deleteTask.textContent = "DELETE";
+        deleteTask.classList.add("bg-blue-500", "rounded-[5px]", "p-2", "text-[10px]", "font-bold", "h-[30px]");
+        taskCard.appendChild(deleteTask);    
+
+        deleteBtnArray.push(deleteTask);
+        console.log(deleteBtnArray);
+
+        btnIdMapping.set(index.itemId, deleteTask);
+    }
 }
 updateDataOnUI();
+
+
+function deletingTasks(){
+    for(let index of localStorageArray){
+        
+    };
+}
+
